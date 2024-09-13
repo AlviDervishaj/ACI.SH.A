42172373016eaaa7bcc7124992c4ac6398cc8d86
Alvi Dervishaj <alvidervishaj9@gmail.com>
2024-09-13 15:01:24 +0200

    Made some improvements to the gallery page and also made the page more responsive.

--- app/[locale]/gallery/page.tsx
+++ app/[locale]/gallery/page.tsx
@@ -1,24 +1,62 @@
+"use client";
 import { Image } from "@nextui-org/image";
-import NextImage from "next/image";
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react";
+import NextImage, { StaticImageData } from "next/image";
+import { useState } from "react";
+import G1 from "../../../public/images/gallery/1.jpeg";
+import G2 from "../../../public/images/gallery/2.jpg";
+import G3 from "../../../public/images/gallery/3.jpg";
+import G4 from "../../../public/images/gallery/4.jpg";
+import G5 from "../../../public/images/gallery/5.jpg";
+import G6 from "../../../public/images/gallery/6.jpg";
+import G7 from "../../../public/images/gallery/7.jpg";
+import G8 from "../../../public/images/gallery/8.jpg";
+import G9 from "../../../public/images/gallery/9.jpg";
+import G10 from "../../../public/images/gallery/10.jpg";
+import G11 from "../../../public/images/gallery/11.jpg";
 
-export default function Home() {
-  const images = [
-    "/images/gallery/1.jpeg",
-    "/images/gallery/2.jpg",
-    "/images/gallery/3.jpg",
-    "/images/gallery/4.jpg",
-    "/images/gallery/5.jpg",
-    "/images/gallery/6.jpg",
-    "/images/gallery/7.jpg",
-    "/images/gallery/8.jpg",
-    "/images/gallery/9.jpg",
-    "/images/gallery/10.jpg",
-    "/images/gallery/11.jpg",
-  ];
+export default function GalleryHomePage() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [image, setImage] = useState<StaticImageData>();
+  const images = [G1, G2, G3, G4, G5, G6, G7, G8, G9, G10, G11,];
+
+  function handleOnOpen(_img: StaticImageData) {
+    setImage(_img);
+    onOpen();
+  }
 
   return (
-    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6">
-      {images.map((image) => <Image key={image} as={NextImage} isZoomed src={image} width={300} height={300} />)}
+    <section className="grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-6 w-full h-full">
+      {images.map((_img) =>
+        <Image onClick={() => handleOnOpen(_img)}
+          key={_img.src} className="cursor-pointer"
+          alt={_img.src} as={NextImage} isZoomed
+          src={_img.src} width={200} height={120}
+        />)}
+      <Modal backdrop={"blur"} isOpen={isOpen} onClose={onClose}
+        classNames={{
+          backdrop: "bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20"
+        }}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">AÇI SH.A Photo</ModalHeader>
+              <ModalBody className="flex flex-row items-center content-center justify-center">
+                {image ?
+                  <Image width={300} height={300} as={NextImage} key={image.src} alt={image.src} src={image.src} />
+                  : null
+                }
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+
+        </ModalContent>
+      </Modal>
     </section >
   );
 }
--- app/[locale]/lubricants/page.tsx
+++ app/[locale]/lubricants/page.tsx
@@ -1,9 +1,72 @@
+"use client";
 import { title } from "@/components/primitives";
+import { usePathname, useRouter } from "@/config/routing";
+import { MockData } from "@/mock_data";
+import { Pagination } from "@nextui-org/react";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { useIsomorphicLayoutEffect } from "usehooks-ts";
+// if 10 pages then get number of items per page
+const itemsPerPage = MockData.length / 10;
 
 export default function LubricantsPage() {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const pathname = usePathname();
+  const router = useRouter();
+  const { replace } = router;
+  const searchParams = useSearchParams();
+
+  useIsomorphicLayoutEffect(() => {
+    const searchParamsCurrentPage = Number(searchParams.get('page')) || 1;
+    setCurrentPage(searchParamsCurrentPage)
+  }, [searchParams])
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
+  function getXItemsFromArray(size: number, itemsToSkip?: number) {
+    setIsLoading(true);
+    const results = [];
+    if (itemsToSkip) {
+      if (itemsToSkip >= MockData.length) {
+        setIsLoading(false);
+        return [];
+      };
+      for (let i = itemsToSkip; i <= size; i++) {
+        results.push(MockData[i]);
+      }
+      setIsLoading(true);
+      console.log({ ITSresults: results });
+      return results;
+    }
+
+    for (let i = 0; i <= size; i++) {
+      results.push(MockData[i]);
+    }
+    setIsLoading(true);
+    console.log({ results });
+    return results;
+  }
+
   return (
     <div>
       <h1 className={title()}>Lubricants</h1>
+      <div className="w-full h-fit p-2"></div>
+      <Pagination
+        total={10}
+        variant="flat"
+        radius="full"
+        showShadow
+        isCompact
+        page={currentPage}
+        onChange={setCurrentPage}
+        color="warning"
+        showControls
+      />
     </div>
   );
 }
